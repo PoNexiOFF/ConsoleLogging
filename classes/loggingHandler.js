3c8d6125334c1988b78d5f2c0ea7165b239f9136
Ponexi <102175430+PoNexiOFF@users.noreply.github.com>
2025-09-22 19:07:50 +0200

    Refactor logging handler and update log usage
    Refactored LogEntry to improve parameter handling and error messaging. Updated main.js to demonstrate various log styles and colors using the new LogEntry API.

--- classes/loggingHandler.js
+++ classes/loggingHandler.js
@@ -2,6 +2,7 @@
 const { LogColor } = require("../constants/LogColor")
 const { getColor } = require("../functions/getColors")
 const { formatDate } = require("../functions/formatTime")
+const { LogStyle } = require("../constants/LogStyle")
 
 /**
 * @typedef {Object} LogOptions
@@ -16,25 +17,27 @@ class LogEntry {
     * @param {LogOptions} options
     */
     constructor ({ text, color, code, style } = {}) {
-        if (!color && !style) {
-            throw new Error("No color or style provided")
-        }
-
-        if (!text) {
-            throw new Error("No text provided")
-        }
-
         this.text = text
-        const colorData = getColor(style?.color || color)
-        if (colorData === false) {
-            throw new Error("No color found")
-        }
-
-        this.color = colorData
+        this.color = color
+        this.style = style
         this.code = code || style?.code || "DEBUG"
     }
 
-    displayError() {
+    display() {
+        if (!this.color && !this.style) {
+            return new LogEntry({ text: "Missing parameters: \"style\" and \"color\"", style: LogStyle.ERROR }).display()
+        }
+
+        if (!this.text) {
+            throw new Error("No text provided")
+        }
+
+        const colorData = getColor(this.style?.color || this.color)
+        if (colorData === false) {
+            throw new Error("No color found")
+        }
+        this.color = colorData
+
         return console.log(`${formatDate(new Date())} ${this.color}[${this.code}]${getColor(LogColor.RESET)} ${this.text}`)
     }
 }
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const { LogEntry, LogColor, LogStyle } = require("./index")
 
-const newLog = new LogEntry({ text: "The log was created successfully and was sent in the console.", style: LogStyle.INFO })
-console.log(newLog)
-newLog.displayError()
+const parameters = [
+    { text: "The code is running!", style: LogStyle.DEBUG },
+    { text: "L'utilisateur \"User\" possède 10 items dans son inventaire.", style: LogStyle.INFO },
+    { text: "Nouvelle mise à jour disponible sur github.com/PoNexiOFF", style: LogStyle.WARNING },
+    { text: "test" },
+    { text: "test", color: LogColor.MAGENTA, code: "BOMBOCLA" }
+]
+
+for (const parameter of parameters) {
+    const newLog = new LogEntry(parameter).display()
+}
